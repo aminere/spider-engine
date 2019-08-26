9d817b252accd9dc4c0e8936b7b620675d9a55e5
Amine Rehioui <amine@franticsoftware.com>
2019-08-26 10:18:54 -0400

    [minor] [graphics] - add mesh getter/setter to StaticMesh

--- src/graphics/geometry/StaticMesh.ts
+++ src/graphics/geometry/StaticMesh.ts
@@ -4,23 +4,45 @@ import { StaticMeshAsset } from "../../assets/StaticMeshAsset";
 import { AssetReference } from "../../serialization/AssetReference";
 import { AABB } from "../../math/AABB";
 import { VertexBuffer } from "../VertexBuffer";
+import { ObjectProps } from "../../core/Types";
+import { SerializedObject } from "../../core/SerializableObject";
 
 export class StaticMesh extends Geometry {
 
-    mesh = new AssetReference(StaticMeshAsset);
+    get version() { return 2; }
+
+    set mesh(mesh: StaticMeshAsset | null) { this._mesh.asset = mesh; }
+    get mesh() { return this._mesh.asset; }
+
+    private _mesh = new AssetReference(StaticMeshAsset);
+
+    constructor(props?: ObjectProps<StaticMesh>) {
+        super();
+        if (props) {
+            this.setState(props);
+        }
+    }
 
     getVertexBuffer(): VertexBuffer | null {
-        let mesh = this.mesh.asset;
-        return mesh ? mesh.vertexBuffer : null;
+        const { asset } = this._mesh;
+        return asset ? asset.vertexBuffer : null;
     }
 
     getBoundingBox(): AABB | null {
-        let mesh = this.mesh.asset;
-        return mesh ? mesh.boundingBox : null;
+        const { asset } = this._mesh;
+        return asset ? asset.boundingBox : null;
     }
 
     destroy() {
-        this.mesh.detach();
+        this._mesh.detach();
         super.destroy();
+    }
+
+    upgrade(json: SerializedObject, previousVersion: number) {
+        if (previousVersion === 1) {
+            Object.assign(json.properties, { _mesh: json.properties.mesh });
+            delete json.properties.mesh;
+        }
+        return json;
     }
 }
