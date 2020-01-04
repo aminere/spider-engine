04fc8748ef63fe9dcebc861f44c8c52c31d93512
Amine Rehioui <amine@franticsoftware.com>
2020-01-04 13:03:37 -0500

    [minor] [behavior] - Provide a functional null implementation for the runtime code generator

--- src/editor/EditorBehaviorExecution.ts
+++ src/editor/EditorBehaviorExecution.ts
@@ -1,11 +1,13 @@
 import { CodeBlock } from "../behavior/CodeBlock";
+import { Debug } from "../io/Debug";
 
 /**
  * @hidden
  */
 export namespace EditorBehaviorExecution {
     export let generateRuntimeCode = (codeBlock: CodeBlock) => {
-        console.assert(false, "Not implemented in this run-time");
+        // Debug.logWarning("EditorBehaviorExecution not implemented in this run-time");
+        codeBlock.sourceInfo = {};
         return "";
     };
 }
