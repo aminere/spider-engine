0a38af953498eab9fcfb60935eb15a5f216fca0e
Amine Rehioui <amine@franticsoftware.com>
2020-01-23 14:53:24 -0500

    iOS launcher wip

---
+++ launcher/ios/ios-launcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-launcher
+//
+//  Created by Amine Rehioui on 2020-01-23.
+//  Copyright © 2020 Frantic Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
