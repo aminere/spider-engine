82c016b8f0625b763dffdda6e1246c21c731ee40
Amine Rehioui <amine@franticsoftware.com>
2020-03-25 21:07:26 -0400

    [minor] [versioning] - Add runtime and assets versioning info

---
+++ src/common/Version.ts
@@ -0,0 +1,9 @@
+
+export class Version {
+    // increment when default assets change
+    public static get assets() { return 25; }
+
+    // increment before cloud deployment
+    // reset to 1 when assets are incremented
+    public static get iteration() { return 2; }
+}
