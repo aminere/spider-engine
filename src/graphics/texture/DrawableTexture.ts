f301925afdd76bedf5833ab7f28cf1fa779e8a89
Amine Rehioui <amine@franticsoftware.com>
2020-06-01 06:36:21 -0400

    [minor] [graphics] - Don't upload DrawableTexture data to GPU if unchanged

--- src/graphics/texture/DrawableTexture.ts
+++ src/graphics/texture/DrawableTexture.ts
@@ -75,7 +75,9 @@ export class DrawableTexture extends Texture {
             }
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-            this.updateStructure();
+            if (this._structureDirty) {
+                this.updateStructure();
+            }
             this.uploadTexturePixels();
         } else {
             gl.activeTexture(gl.TEXTURE0 + stage);
