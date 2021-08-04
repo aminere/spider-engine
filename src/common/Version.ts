194fad9da5cc8ba0b539659797645a6b063035a7
Amine Rehioui <amine@franticsoftware.com>
2021-08-04 10:31:44 +0200

    Version 26.1 / updated fullscreen shader

--- src/common/Version.ts
+++ src/common/Version.ts
@@ -1,9 +1,9 @@
 
 export class Version {
     // increment when default assets change
-    public static get assets() { return 25; }
+    public static get assets() { return 26; }
 
     // increment before cloud deployment
     // reset to 1 when assets are incremented
-    public static get iteration() { return 8; }
+    public static get iteration() { return 1; }
 }
